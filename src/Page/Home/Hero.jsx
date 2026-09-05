1c6c4e4901fc57ce58ab3b17aad1003ec1053ac1
rintymoni01 <rintymoni01@gmail.com>
2026-09-05 12:20:22 +0600

    responsived

--- src/Page/Home/Hero.jsx
+++ src/Page/Home/Hero.jsx
@@ -9,9 +9,9 @@ const Hero = () => {
         >
 
             <div>
-                <div className="text-green-300 drop-shadow-md text-center pt-20">
+                <div className="text-green-300 lg:pt-20 drop-shadow-md text-center pt-40">
                     <p className="text-xl font-semibold text-gray-200">Set out in search of your next adventure </p>
-                    <p className="text-4xl font-bold pt-6">Nature is calling— <br />are you ready
+                    <p className="lg:text-4xl  lg:font-bold  text-2xl font-semibold pt-6">Nature is calling— <br />are you ready
                          to answer the call?</p>
                 </div>
         <div className="">
