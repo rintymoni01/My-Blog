e8d1dd92076c19a51068f5468654aa947b4d2ce7
rintymoni01 <rintymoni01@gmail.com>
2026-09-05 13:18:08 +0600

    responsived

--- src/Page/Home/Hero.jsx
+++ src/Page/Home/Hero.jsx
@@ -4,26 +4,26 @@ import bg from '../../assets/bg.jpg';
 const Hero = () => {
     return (
         <div
-            className="h-[600px]  bg-cover bg-center"
+            className="h-[600px] sm:h-[500px] md:h-[550px] lg:h-screen bg-cover bg-center bg-no-repeat"
             style={{ backgroundImage: `url(${bg})` }}
         >
 
-            <div>
-                <div className="text-green-300 lg:pt-20 drop-shadow-md text-center pt-40">
+            <div className='bg-black/25 '>
+                <div className="lg:pt-60 drop-shadow-md text-center pt-60">
                     <p className="text-xl font-semibold text-gray-200">Set out in search of your next adventure </p>
-                    <p className="lg:text-4xl  lg:font-bold  text-2xl font-semibold pt-6">Nature is calling— <br />are you ready
+                    <p className="lg:text-4xl  lg:font-bold  text-2xl text-white font-semibold pt-6">Nature is calling— <br />are you ready
                          to answer the call?</p>
                 </div>
         <div className="">
      <div className="lg:pt-75 pt-40 pl-12">
-            <div className="border rounded-md mx-auto  lg:mx-0 text-white w-36 hover:text-black   hover:bg-white hover:font-semibold duration-1000 bg-gray-400 border-gray-300 px-6 py-1">
+            <div className=" rounded-md mx-auto font-semibold  lg:mx-0  w-36 hover:text-white bg-pink-500 hover:font-semibold duration-1000  shadow-2xl  px-6 py-1 hover:bg-black text-white">
   Explore Now
 </div>
      </div>
 
 <div className="hidden lg:block absolute right-6 top-1/2 -translate-y-1/2 
-border rounded-md text-white  bg-black font-semibold border-gray-300 
-px-6 py-1 rotate-90">
+border rounded-md text-white   bg-black font-semibold border-gray-300 
+px-6 py-1 rotate-90 ">
   <u><i>Rinty moni..</i></u>
 </div>
  </div>
