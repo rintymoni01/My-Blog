e26e5287775628c593d352829e0c96d953abb26f
rintymoni01 <rintymoni01@gmail.com>
2026-09-07 12:09:58 +0600

    location,category,likes,comments,reating

--- src/Components/Shared/SheardBlogCard.jsx
+++ src/Components/Shared/SheardBlogCard.jsx
@@ -5,6 +5,13 @@ const SheardBlogCard = ({blog}) => {
         <div>
             <div>
                 <img className='w-52 h-42' src={blog.image} alt="" />
+                <h3>{blog.category}</h3>
+                <h3>{blog.location}</h3>
+                 
+                  <h3>{blog.category}</h3>
+                   <h3>{blog.likes}</h3>
+                   <p>{blog.rating}</p>
+                   <p>{blog.comments}</p>
             </div>
         </div>
     );
