0c3740907613a6311a5582b2feac7846329a5ffa
rintymoni01 <rintymoni01@gmail.com>
2026-09-08 12:50:26 +0600

    link

--- src/Page/Home/HomeComponents/Dicplay.jsx
+++ src/Page/Home/HomeComponents/Dicplay.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import dicplay from "../../../assets/dicplay.jfif";
+import { Link } from "react-router";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const Dicplay = () => {
   return (
@@ -13,6 +15,12 @@ const Dicplay = () => {
         Sometimes, all you need is a little time <br /> away from the rush of life.
       </p>
       <p className="text-xl font-semibold pl-26 text-gray-200">Explore Your Journey →</p>
+      <Link to="/about">
+      <button className="px-2 py-1 flex gap-1 items-center text-white bg-pink-400 rounded-md">
+        <p className="font-semibold ">Explore Now</p>
+        <FaArrowRightLong/>
+      </button>
+      </Link>
       </div>
     </div>
   );
