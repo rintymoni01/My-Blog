7059b826c52bd52b3b825eaefdfb3ffeb3a849e5
rintymoni01 <rintymoni01@gmail.com>
2026-09-09 11:40:06 +0600

    add data

--- src/Components/Shared/SingleBlogs.jsx
+++ src/Components/Shared/SingleBlogs.jsx
@@ -11,13 +11,26 @@ const SingleBlogs = () => {
     return (
         <div className='items-center container mx-auto px-4 py-8'>
             <div>
-                <img  className='w-70 h-70 rounded-t-md'
+                <img  className='w-70 h-70 rounded-t-md '
                     src={findBlogs?.image} 
                     alt={""} 
                 />
             </div>
 
-            <p>{findBlogs?.title}</p>
+             
+            <p>{findBlogs?.location}</p>
+            <p>{findBlogs.longDescription}</p>
+            <div className='flex gap-4  items-center'>
+              <p>duration: {findBlogs?.duration}</p>
+            <p>difficulty: {findBlogs?.difficulty}</p>
+            </div>
+            <p>bestTime: {findBlogs?.bestTime}</p>
+            <p>{findBlogs?.tags}</p>
+           <div className='flex gap-4'>
+             <p>{findBlogs?.transport}</p>
+            <p>{findBlogs?.budget}</p>
+           </div>
+
         </div>
     );
 };
