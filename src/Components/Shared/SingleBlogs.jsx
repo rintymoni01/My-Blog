0d2fa3af2b04d08cb0d095b72dd75373b57a20c0
rintymoni01 <rintymoni01@gmail.com>
2026-09-09 11:53:57 +0600

    color,h.w,font, px,py-pt

--- src/Components/Shared/SingleBlogs.jsx
+++ src/Components/Shared/SingleBlogs.jsx
@@ -10,26 +10,30 @@ const SingleBlogs = () => {
 
     return (
         <div className='items-center container mx-auto px-4 py-8'>
-            <div>
+          <div className='flex gap-12'>
+              <div>
                 <img  className='w-70 h-70 rounded-t-md '
                     src={findBlogs?.image} 
                     alt={""} 
                 />
             </div>
 
-             
-            <p>{findBlogs?.location}</p>
-            <p>{findBlogs.longDescription}</p>
+          <div className='w-145'>
+               
+            <p className='text-gray-800 font-semibold text-xl'>{findBlogs?.location}</p>
+            <p className='text-xs font-bold text-gray-500'>{findBlogs.longDescription}</p>
             <div className='flex gap-4  items-center'>
-              <p>duration: {findBlogs?.duration}</p>
-            <p>difficulty: {findBlogs?.difficulty}</p>
+              <p className='text-[#007d8e] font-bold '>duration: <span className='text-gray-600 font-semibold '>{findBlogs?.duration}</span></p>
+            <p className='text-[#007d8e] font-bold '>difficulty: <span className='text-gray-600 font-semibold '>{findBlogs?.difficulty}</span></p>
             </div>
-            <p>bestTime: {findBlogs?.bestTime}</p>
+            <p className='text-[#007d8e] font-bold'>bestTime: <span className='text-gray-600 font-semibold '> {findBlogs?.bestTime}</span></p>
             <p>{findBlogs?.tags}</p>
            <div className='flex gap-4'>
              <p>{findBlogs?.transport}</p>
             <p>{findBlogs?.budget}</p>
            </div>
+          </div>
+          </div>
 
         </div>
     );
