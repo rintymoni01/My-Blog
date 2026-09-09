4747d6a0598d9f9dd2e8296955e9ca09029d5206
rintymoni01 <rintymoni01@gmail.com>
2026-09-09 17:54:46 +0600

    add img

--- src/Components/Shared/SingleBlogs.jsx
+++ src/Components/Shared/SingleBlogs.jsx
@@ -16,17 +16,17 @@ const SingleBlogs = () => {
       <div className="flex gap-12 justify-center">
         <div>
           <img
-            className="w-111 h-70 rounded-t-md "
+            className="w-111 h-70 rounded-md "
             src={findBlogs?.image}
             alt={""}
           />
-       <div className="flex gap-3 pt-3 items-center justify-center ">
-           {categories?.slice(0,3) .map((c) => (
-        <div key={c.id}>
-         <img className="h-28 w-35" src={c?.image} alt="" />
-        </div>
-      ))}
-       </div>
+          <div className="flex gap-3 pt-3 items-center justify-center ">
+            {categories?.slice(0, 3).map((c) => (
+              <div key={c.id}>
+                <img className="h-28 w-35" src={c?.image} alt="" />
+              </div>
+            ))}
+          </div>
         </div>
 
         <div className="w-145">
@@ -37,43 +37,45 @@ const SingleBlogs = () => {
           <p className="text-sm font-semibold text-gray-400  pb-2">
             {findBlogs?.longDescription}
           </p>
-          
 
           <div className="flex gap-4  items-center py-1">
             <p className="text-[#007d8e] font-bold ">
-              Duration:{" "}
+             Duration:  <space></space>
               <span className="text-gray-600 font-semibold ">
                 {findBlogs?.duration}
               </span>
             </p>
             <p className="text-[#007d8e] font-bold ">
-              Difficulty:{" "}
+              Difficulty:  <space></space>
               <span className="text-gray-600 font-semibold ">
                 {findBlogs?.difficulty}
               </span>
             </p>
           </div>
           <p className="text-[#007d8e] font-bold">
-            BestTime:{" "}
+            BestTime: <space></space>
             <span className="text-gray-600 font-semibold ">
-              {" "}
               {findBlogs?.bestTime}
             </span>
           </p>
           <div className="flex py-1 gap-4">
             <p className="text-[#007d8e] font-bold">{findBlogs?.transport}:</p>
             <p className="text-gray-600 font-semibold  ">
-              {" "}
               <u>{findBlogs?.budget}</u>
             </p>
-            
           </div>
-          <div className="flex gap-1 pb-1 items-center">
-            <p className="text-[#007d8e] font-bold">Reating:</p>
-            <TiStarFullOutline className="text-orange-500" />
-            <TiStarFullOutline className="text-orange-500" />
-            <TiStarFullOutline className="text-orange-500" />
-            <TiStarFullOutline className="text-orange-500" />
+          <div className="flex  pb-1 items-center">
+            <p className="text-[#007d8e] font-bold  items-center">Reating:</p>
+            <space></space>
+            <TiStarFullOutline className="text-orange-500 text-xl" />
+            <TiStarFullOutline className="text-orange-500 text-xl" />
+            <TiStarFullOutline className="text-orange-500 text-xl" />
+            <TiStarFullOutline className="text-orange-500 text-xl" />
+          </div>
+          <div className="flex gap-4 pb-1">
+            <p className="text-[#007d8e] font-bold ">Likes: <span className="text-gray-400 font-semibold ">{findBlogs?.likes}</span></p>
+            <p className="text-[#007d8e] font-bold ">Comments: <span className="text-gray-400 font-semibold ">{findBlogs?.comments}</span></p>
+            <p className="text-[#007d8e] font-bold ">Views: <span className="text-gray-400  font-semibold "> {findBlogs?.views}</span></p>
           </div>
           <div className="py-2 flex gap-2 flex-wrap">
             {findBlogs?.tags?.map((t, index) => (
@@ -111,9 +113,15 @@ const SingleBlogs = () => {
         </div>
       </div>
       <div>
-        <p>jdfkydfdsf</p>
+        
       </div>
-      
+<div  className="flex ">
+  {categories?.slice(7,10).map((c) => (
+              <div key={c.id}>
+                <img className="h-30 w-50" src={c?.image} alt="" />
+              </div>
+            ))}
+</div>
     </div>
   );
 };
