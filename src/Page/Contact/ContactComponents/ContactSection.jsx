fda907d5e1df1ca0c97bb3070c7059d7408d67d8
rintymoni01 <rintymoni01@gmail.com>
2026-09-10 20:27:54 +0600

    contact

--- src/Page/Contact/ContactComponents/ContactSection.jsx
+++ src/Page/Contact/ContactComponents/ContactSection.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
-
+import React from "react";
 const ContactSection = () => {
-    return (
-        <div>
-            gfhg
-        </div>
-    );
+  return (
+    <div>
+      <div className="text-center">
+        <h1 className="text-2xl font-bold">
+          Contact <span className="text-blue-500">With Us</span>
+        </h1>
+        <p>Get in touch with us and let’s make your journey unforgettable.</p>
+      </div>
+    </div>
+  );
 };
 
 export default ContactSection;
