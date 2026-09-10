4d1a2c21c9843d74c12193f9702ed2e477925f1e
rintymoni01 <rintymoni01@gmail.com>
2026-09-10 22:14:12 +0600

    add map location

--- src/Page/Contact/ContactComponents/ContactSection.jsx
+++ src/Page/Contact/ContactComponents/ContactSection.jsx
@@ -4,7 +4,6 @@ import {
   MdEmail,
   MdLocationOn,
   MdMarkEmailRead,
-
   MdOutlinePhoneAndroid,
 } from "react-icons/md";
 const ContactSection = () => {
@@ -53,13 +52,26 @@ const ContactSection = () => {
           </div>
           <p className="text-gray-700 text-sm font-bold pt-1">Address</p>
           <div className="flex items-center gap-1 py-1">
-            <MdLocationOn  className="text-gray-500 text-xs" />
-            <p className="text-xs text-gray-500">House 12, Road 3, Mohakhali, Dhaka 1212, Bangladesh</p>
+            <MdLocationOn className="text-gray-500 text-xs" />
+            <p className="text-xs text-gray-500">
+              House 12, Road 3, Mohakhali, Dhaka 1212, Bangladesh
+            </p>
           </div>
-          
         </div>
       </div>
-      
+      <div>
+        <div>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.4851008789146!2d90.42419807532019!3d23.7657340881875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c754583dd209%3A0xdd0c5fcc7d2d3836!2sBetopia%20Group%20-%20Corporate%20Office!5e0!3m2!1sen!2sbd!4v1789056770328!5m2!1sen!2sbd"
+            width="600"
+            height="450"
+            style={{border:0}}
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="strict-origin-when-cross-origin"
+          ></iframe>
+        </div>
+      </div>
     </div>
   );
 };
