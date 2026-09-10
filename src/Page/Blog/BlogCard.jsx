fe353c1ac73f53621ec41a8676322731fc0d7d05
rintymoni01 <rintymoni01@gmail.com>
2026-09-10 18:52:02 +0600

    icons

--- src/Page/Blog/BlogCard.jsx
+++ src/Page/Blog/BlogCard.jsx
@@ -6,8 +6,36 @@ import { Link } from 'react-router';
 
 const BlogCard = () => {
     return (
-       <div>
-         <div className="container mx-auto px-4 py-16">
+       <div className='py-8 bg-gray-200'>
+     
+       <div
+  className="h-[45vh] sm:h-[50vh] lg:h-[70vh] w-full lg:w-360 bg-cover bg-center bg-no-repeat"
+  style={{ backgroundImage: `url("/Image/dicplay.jfif")` }}
+>
+  <div className="h-full flex flex-col justify-center items-center lg:items-start py-10 lg:py-0">
+    
+    <p className="text-lg sm:text-xl lg:text-2xl text-white font-bold text-center lg:text-left lg:pl-26">
+      Pause Life. Find Yourself.
+    </p>
+
+    <p className="text-2xl sm:text-3xl lg:text-4xl font-bold text-gray-200 py-3 lg:py-4 text-center lg:text-left lg:pl-26">
+      Sometimes, all you need is a little time <br className="hidden lg:block" />
+      away from the rush of life.
+    </p>
+
+    <div className="lg:pl-26">
+      <Link to="/about">
+        <button className="px-5 sm:px-6 py-2 flex gap-1 items-center text-white bg-pink-100 rounded-4xl">
+          <p className="text-base sm:text-lg lg:text-xl font-semibold text-black">
+            Explore Your Journey →
+          </p>
+        </button>
+      </Link>
+    </div>
+
+  </div>
+</div>
+    <div className="container mx-auto px-4 py-16">
   <div className="flex flex-col lg:flex-row justify-center items-center gap-4">
 
     {/* Road Trip */}
@@ -66,33 +94,6 @@ const BlogCard = () => {
           <p><GoStarFill /></p>
         </div>
       </div>
-    </div>
-
-  </div>
-</div>
-       <div
-  className="h-[45vh] sm:h-[50vh] lg:h-[70vh] w-full lg:w-360 bg-cover bg-center bg-no-repeat"
-  style={{ backgroundImage: `url("/Image/dicplay.jfif")` }}
->
-  <div className="h-full flex flex-col justify-center items-center lg:items-start py-10 lg:py-0">
-    
-    <p className="text-lg sm:text-xl lg:text-2xl text-white font-bold text-center lg:text-left lg:pl-26">
-      Pause Life. Find Yourself.
-    </p>
-
-    <p className="text-2xl sm:text-3xl lg:text-4xl font-bold text-gray-200 py-3 lg:py-4 text-center lg:text-left lg:pl-26">
-      Sometimes, all you need is a little time <br className="hidden lg:block" />
-      away from the rush of life.
-    </p>
-
-    <div className="lg:pl-26">
-      <Link to="/about">
-        <button className="px-5 sm:px-6 py-2 flex gap-1 items-center text-white bg-pink-100 rounded-4xl">
-          <p className="text-base sm:text-lg lg:text-xl font-semibold text-black">
-            Explore Your Journey →
-          </p>
-        </button>
-      </Link>
     </div>
 
   </div>
--- src/Page/Blog/Blogpera.jsx
+++ src/Page/Blog/Blogpera.jsx
@@ -2,11 +2,25 @@ import React from 'react';
 
 const Blogpera = () => {
     return (
-        <div className='text-center pt-10'>
-                    <p className="font-semibold text-gray-500">Explore new destinations, create unforgettable memories, and write your own adventure story.</p>
-        <p className="text-xl font-semibold text-gray-700 py-1">Explore new places, make unforgettable memories.</p>
-<h1 className="font-bold text-2xl pb-12 ">Discover the Adventure Within</h1>
-        </div>
+<div className="text-center py-6 sm:pt-5 lg:pt-10 px-3 flex flex-col">
+
+  {/* Last → First */}
+  <h1 className="order-1 lg:order-3 font-bold text-xl sm:text-2xl lg:text-2xl pb-2 sm:pb-4 lg:pb-12">
+    Discover the Adventure Within
+  </h1>
+
+  {/* Middle → Middle */}
+  <p className="order-2 lg:order-2 text-base sm:text-lg lg:text-xl font-semibold text-gray-700 py-1 sm:py-2">
+    Explore new places, make unforgettable memories.
+  </p>
+
+  {/* First → Last */}
+  <p className="order-3 lg:order-1 font-semibold text-sm sm:text-base text-gray-500 leading-relaxed max-w-2xl mx-auto">
+    Explore new destinations, create unforgettable memories, write your own adventure story.
+  </p>
+
+</div>
+
     );
 };
 
